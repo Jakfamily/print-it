f0812c3a0dbefe8c0725ae1fff62622c68a315bb
jakfamily <fariajeanbap@gmail.com>
2023-12-21 11:49:27 +0100

    ajout de commentaire

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// Définition des diapositives
 const slides = [
   {
     image: "slide1.jpg",
@@ -18,25 +19,32 @@ const slides = [
   },
 ];
 
+// Récupération des éléments HTML pour les flèches gauche et droite
 const arrowLeft = document.getElementById("arrowLeft");
 const arrowRight = document.getElementById("arrowRight");
 
+// Gestionnaire d'événement pour la flèche gauche
 arrowLeft.addEventListener("click", function () {
   console.log("Clic sur le bouton gauche");
 });
 
+// Gestionnaire d'événement pour la flèche droite
 arrowRight.addEventListener("click", function () {
   console.log("Clic sur le bouton droit");
 });
 
+// Calcul du nombre total de diapositives
 const nombreDeSlides = slides.length;
 
+// Récupération du conteneur des points (dots)
 const DotsContainer = document.getElementById("DotsContainer");
 
+// Création des points (dots) et ajout au conteneur
 for (let i = 0; i < nombreDeSlides; i++) {
   const dot = document.createElement("span");
   dot.classList.add("dot");
 
+  // Ajout de la classe "dot_selected" au premier dot
   if (i === 0) {
     dot.classList.add("dot_selected");
   }
@@ -44,25 +52,33 @@ for (let i = 0; i < nombreDeSlides; i++) {
   DotsContainer.appendChild(dot);
 }
 
+// Sélection de tous les points (dots) créés
 const dots = document.querySelectorAll(".dot");
 
+// Gestionnaire d'événement pour chaque point (dot)
 dots.forEach((dot, index) => {
   dot.addEventListener("click", () => {
     console.log("Clique sur une diapo");
   });
 });
 
+// Initialisation de l'index courant
 let currentIndex = 0;
 
+// Récupération des éléments image et tagline
 const imageElement = document.getElementById("img_banner");
 const tagLineElement = document.querySelector("#tagline");
+
+// Récupération de tous les points (dots)
 const bulletPoints = document.querySelectorAll(".dot");
 
+// Fonction pour mettre à jour le carousel
 function updateCarousel() {
+  // Mise à jour de l'image et du tagline en fonction de l'index courant
   imageElement.src = "./assets/images/slideshow/" + slides[currentIndex].image;
-
   tagLineElement.innerHTML = slides[currentIndex].tagLine;
 
+  // Mise à jour des classes des points (dots) en fonction de l'index courant
   dots.forEach((dot, index) => {
     if (index === currentIndex) {
       dot.classList.add("dot_selected");
@@ -72,15 +88,19 @@ function updateCarousel() {
   });
 }
 
+// Gestionnaire d'événement pour la flèche droite
 arrowRight.addEventListener("click", () => {
   currentIndex = (currentIndex + 1) % slides.length;
+  // Appel de la fonction pour mettre à jour le carousel
   updateCarousel();
 });
 
+// Gestionnaire d'événement pour la flèche gauche
 arrowLeft.addEventListener("click", () => {
   currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  // Appel de la fonction pour mettre à jour le carousel
   updateCarousel();
 });
 
-// Appel initial pour afficher la première diapositive
+// Appel initial de la fonction pour afficher la première diapositive
 updateCarousel();
